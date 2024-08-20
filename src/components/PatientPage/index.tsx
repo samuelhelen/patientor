eba36e58cae8069f14818de70501bc8f9806eddb
Samuel Helén <samuel.helen@protonmail.com>
2024-08-20 15:24:06 +0300

    Finish 9.21

---
+++ src/components/PatientPage/index.tsx
@@ -0,0 +1,49 @@
+import { Box, Typography } from "@mui/material";
+// the commented out stuff if you want to replace gender texts with icons:
+// https://mui.com/material-ui/material-icons/
+// import {} from "@mui/icons-material"
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+import { Patient } from "../../types";
+
+import patientService from "../../services/patients";
+
+const PatientPage = () => {
+  const { patientId } = useParams();
+
+  const [patient, setPatient] = useState<Patient | null>(null);
+
+  useEffect(() => {
+    if (typeof patientId !== "string") {
+      return;
+    }
+
+    const fetchPatient = async () => {
+      const newPatient = await patientService.getById(patientId);
+      // TODO: handle error responses
+
+      setPatient(newPatient);
+    };
+    fetchPatient();
+  }, [patientId]);
+
+  // TODO: handle loading and error notification separately:
+  if (!patient) {
+    return <div>Loading or patient loading failed</div>;
+  }
+
+  return (
+    <div className="App">
+      <Box>
+        <Typography align="left" variant="h5">
+          {patient.name} ({patient.gender})
+        </Typography>
+        {patient.ssn && <div>ssh: {patient.ssn}</div>}
+        <div>occupation: {patient.occupation}</div>
+      </Box>
+    </div>
+  );
+};
+
+export default PatientPage;
